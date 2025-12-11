f7785fa153c409510e7c3b5bf682187590b719b1
Christian Lauinger <christian.lauinger@gmail.com>
2025-12-11 07:34:22 +0100

    Cleans up localization files and updates packaging.
    Removes unnecessary localization files and updates the packaging process.
    
    The removal of `locale/locale/ru/LC_MESSAGES/gamemodeshellextension@trsnaqe.com.mo`, `po/LINGUAS`, and `po/Makefile.in.in`, `po/POTFILES.in` indicates a cleanup of the localization setup.
    
    Updates the `makezip.sh` script to correctly package the extension with gettext support by adding the `--podir` option.
    The `metadata.json` file is updated to include the `gettext-domain` field which identifies the translation domain for the extension.

--- po/update_strings.sh
+++ po/update_strings.sh
@@ -2,6 +2,6 @@
 SCRIPTDIR=`dirname $0`
 xgettext  --from-code=UTF-8 -k_ -kN_  -o gamemode-shell-extension.pot "$SCRIPTDIR"/../*.js "$SCRIPTDIR"/../settings/*.xml
 
-for fn in *.po; do
-	msgmerge -U "$fn" gamemode-shell-extension.pot
+for pofile in *.po; do
+	msgmerge --backup=off -N -U "$pofile" gamemode-shell-extension.pot
 done
